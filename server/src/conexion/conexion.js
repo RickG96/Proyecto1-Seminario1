fec1c2e6e016a995aa1b48e9d32b260e9346d08a
RickG96 <30740904+RickG96@users.noreply.github.com>
2021-07-09 10:25:52 -0600

    Update conexion.js

--- server/src/conexion/conexion.js
+++ server/src/conexion/conexion.js
@@ -3,19 +3,19 @@ let awsKeys = {
     DynamoDB: {
         region: 'us-east-2',
         endpoint: 'http://dynamodb.us-east-2.amazonaws.com',
-        accessKeyId: 'AKIA4NNOQMEWLXGJJRMA',
-        secretAccessKey: '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' 
+        accessKeyId: '*****************JRMA',
+        secretAccessKey: '*****************************PWnuhj/q0' 
     },
     S3: {
         region: 'us-east-2',
-        accessKeyId: 'AKIA4NNOQMEWESFZGMEX',
-        secretAccessKey: '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'
+        accessKeyId: '*************************GMEX',
+        secretAccessKey: '******************************umLmXAuh'
     },
     Rekognition: {
         region: 'us-east-2',
         apiVersion: '2016-06-27',
-        accessKeyId: 'AKIA4NNOQMEWAOMZXZSM',
-        secretAccessKey: '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'
+        accessKeyId: '****************XZSM',
+        secretAccessKey: '*****************************/usH+b'
     }
 
 }
